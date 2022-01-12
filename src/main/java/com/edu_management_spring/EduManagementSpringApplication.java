a4cc7e9706175ca8bd0713a9f0f59cb571109485
Shafayet <skilift14@gmail.com>
2022-01-12 19:40:08 +0600

    2nd Commit 12/01/2022

--- src/main/java/com/edu_management_spring/EduManagementSpringApplication.java
+++ src/main/java/com/edu_management_spring/EduManagementSpringApplication.java
@@ -8,17 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-@RestController
 public class EduManagementSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EduManagementSpringApplication.class, args);
     }
 
-    @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
-
-    }
+//    @GetMapping("/hello")
+//    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+//        return String.format("Hello %s!", name);
+//
+//    }
 
 }
